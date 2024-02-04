3cb616d2c7831b31c5262910a75e0aec07de0d7b
Raúl Roa <raulroa@Rauls-Mac-mini.local>
2024-02-04 14:25:09 -0400

    Adds new method to handle Get requests in the HttpApiClient and the TagshelfDomain property in the IAlfred interface

--- AlfredApiWrapper/Core/HttpApiClient.cs
+++ AlfredApiWrapper/Core/HttpApiClient.cs
@@ -10,6 +10,7 @@ namespace TagShelf.Alfred.ApiWrapper.Core
     public class HttpApiClient
     {
         private readonly HttpClient _httpClient;
+        private readonly string _clientIdentifier;
 
         /// <summary>
         /// Initializes a new instance of the HttpApiClient class.
@@ -17,7 +18,8 @@ namespace TagShelf.Alfred.ApiWrapper.Core
         public HttpApiClient()
         {
             _httpClient = new HttpClient();
-            // Initialize your HttpClient instance here
+            _clientIdentifier = Guid.NewGuid().ToString(); // Unique identifier for this client instance
+            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd($"dotnet-alfred-api-wrapper/{_clientIdentifier}");
         }
 
         /// <summary>
@@ -52,6 +54,22 @@ namespace TagShelf.Alfred.ApiWrapper.Core
         public async Task<HttpResponseMessage> PostAsync(string uri, HttpContent content)
         {
             return await _httpClient.PostAsync(uri, content);
+        }
+
+        /// <summary>
+        /// Sends a GET request to the specified Uri and returns the response body as a string.
+        /// </summary>
+        /// <param name="uri">The Uri the request is sent to.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the response body as a string.</returns>
+        public async Task<string> GetAsync(string uri)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
